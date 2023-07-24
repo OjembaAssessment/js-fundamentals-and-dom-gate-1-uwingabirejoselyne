3d442de00edd88e891f919ea6ca068f51abeb4f0
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-07-24 13:06:44 +0200

    some change

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -17,68 +17,71 @@ function startTimer() {
       stopTimer();
     }
   }, 1000);
+ let start=document.getElementsByClassName("start-stop")
+ let add=document.getElementsByClassName("add")
+ let subtract=document.getElementsByClassName("subtract")
+ let reset= document.getElementsByClassName("reset")
+  start.textContent = "Stop";
+  add.disabled = true;
+  subtract.disabled = true;
+  reset.disabled = true;
 
-  document.querySelector(".start-stop").textContent = "Stop";
-  document.querySelector(".add").disabled = true;
-  document.querySelector(".subtract").disabled = true;
-  document.querySelector(".reset").disabled = true;
+  reset.classList.add("disabled");
+  add.classList.add("disabled");
+  subtract.classList.add("disabled");
 
-  document.querySelector(".reset").classList.add("disabled");
-  document.querySelector(".add").classList.add("disabled");
-  document.querySelector(".subtract").classList.add("disabled");
+  reset.style.backgroundColor = "#ccc";
+  add.style.backgroundColor = "#ccc";
+  subtract.style.backgroundColor = "#ccc";
 
-  document.querySelector(".reset").style.backgroundColor = "#ccc";
-  document.querySelector(".add").style.backgroundColor = "#ccc";
-  document.querySelector(".subtract").style.backgroundColor = "#ccc";
-
-  document.querySelector(".reset").style.cursor = "not-allowed";
-  document.querySelector(".add").style.cursor = "not-allowed";
-  document.querySelector(".subtract").style.cursor = "not-allowed";
+  reset.style.cursor = "not-allowed";
+  add.style.cursor = "not-allowed";
+  subtract.style.cursor = "not-allowed";
 }
 
 function stopTimer() {
   clearInterval(intervalId);
   isCounting = false;
 
-  document.querySelector(".start-stop").textContent = "Start";
-  document.querySelector(".add").disabled = false;
-  document.querySelector(".subtract").disabled = false;
+  start.textContent = "Start";
+  add.disabled = false;
+  subtract.disabled = false;
 
   if (seconds === 0) {
-    document.querySelector(".reset").disabled = true;
-    document.querySelector(".reset").classList.add("disabled");
-    document.querySelector(".reset").style.backgroundColor = "#ccc";
-    document.querySelector(".reset").style.cursor = "not-allowed";
+    reset.disabled = true;
+    reset.classList.add("disabled");
+    reset.style.backgroundColor = "#ccc";
+    reset.style.cursor = "not-allowed";
   } else {
-    document.querySelector(".reset").disabled = false;
-    document.querySelector(".reset").classList.remove("disabled");
-    document.querySelector(".reset").style.backgroundColor = "#f44336";
-    document.querySelector(".reset").style.cursor = "pointer";
+    document.getElementsByClassName("reset").disabled = false;
+    document.getElementsByClassName("reset").classList.remove("disabled");
+    document.getElementsByClassName("reset").style.backgroundColor = "#f44336";
+    document.getElementsByClassName("reset").style.cursor = "pointer";
   }
 
-  document.querySelector(".add").classList.remove("disabled");
-  document.querySelector(".subtract").classList.remove("disabled");
-  document.querySelector(".add").style.backgroundColor = "#2196F3";
-  document.querySelector(".subtract").style.backgroundColor = "#2196F3";
-  document.querySelector(".add").style.cursor = "pointer";
-  document.querySelector(".subtract").style.cursor = "pointer";
+  document.getElementsByClassName("add").classList.remove("disabled");
+  document.getElementsByClassName("subtract").classList.remove("disabled");
+  document.getElementsByClassName("add").style.backgroundColor = "#2196F3";
+  document.getElementsByClassName("subtract").style.backgroundColor = "#2196F3";
+  document.getElementsByClassName("add").style.cursor = "pointer";
+  document.getElementsByClassName("subtract").style.cursor = "pointer";
 }
 
 function resetTimer() {
   if (!isCounting && seconds === 0) {
-    document.querySelector(".start-stop").disabled = true;
-    document.querySelector(".start-stop").classList.add("disabled");
-    document.querySelector(".start-stop").style.backgroundColor = "#ccc";
+    document.getElementsByClassName("start-stop").disabled = true;
+    document.getElementsByClassName("start-stop").classList.add("disabled");
+    document.getElementsByClassName("start-stop").style.backgroundColor = "#ccc";
 
     return;
   }
 
   seconds = 0;
   updateDisplay();
-  document.querySelector(".reset").disabled = true;
-  document.querySelector(".reset").classList.add("disabled");
-  document.querySelector(".reset").style.backgroundColor = "#ccc";
-  document.querySelector(".reset").style.cursor = "not-allowed";
+  document.getElementsByClassName("reset").disabled = true;
+  document.getElementsByClassName("reset").classList.add("disabled");
+  document.getElementsByClassName("reset").style.backgroundColor = "#ccc";
+  document.getElementsByClassName("reset").style.cursor = "not-allowed";
 }
 
 function addSecond() {
