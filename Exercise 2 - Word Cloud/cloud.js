362521c59217357f142d6ade3409ed05a33d1406
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-07-24 12:07:37 +0200

    splitting paragraph words

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,5 +1,19 @@
 
-const paragraphText = document.getElementById("myParagraph").innerText;
-const words = paragraphText.split(" ");
+const paragraph = document.getElementById("myParagraph").innerText;
+const words = paragraph.split(" ");
+const wordNumber = {};
+words.forEach((word) => {
+  const lowercase = word.toLowerCase();
+  if (wordNumber[lowercase]) {
+    wordNumber[lowercase]++;
+  } else {
+    wordNumber[lowercase] = 1;
+  }
+});
+
+
+const sortedWords = Object.keys(wordNumber).sort(
+  (a, b) => wordNumber[b] - wordNumber[a]
+);
 
 
