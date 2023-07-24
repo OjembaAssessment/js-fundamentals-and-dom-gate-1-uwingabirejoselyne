c652c53e6411cc7edaa9288165c3cf014a7d9c21
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-07-24 10:48:48 +0200

    working on button

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,27 @@
 console.log("Exercise 3 - Timer");
+var counter =29
+var sec = setInterval(function(){
+    counter --
+    let value = document.getElementById('second')
+    value.textContent = counter
+    if(counter<=0){
+        clearInterval(sec);
+    }
+},1000);
+
+var minus =document.getElementById('minus');
+var add =document.getElementById('add');
+var reset =document.getElementById('reset');
+
+
+minus.addEventListener('click',function(){
+    counter =counter-1;
+})
+add.addEventListener('click',function(){
+    counter =counter+1;
+})
+reset.addEventListener('click',function(){
+    counter =0;
+})
+
+
